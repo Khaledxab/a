2e2285a862b2257a7fb6a5572f5127ed523843b6
khaledxab <khaledxab@gmail.com>
2022-08-20 23:30:44 +0100

    script start

--- markdown2html.py
+++ markdown2html.py
@@ -7,26 +7,25 @@ import re
 import hashlib
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print("Usage: ./markdown2html.py <markdown_file> <html_file>")
-		exit(1)
-	if not os.path.isfile(sys.argv[1]):
-		print("Missing {}".format(sys.argv[1]))
-		exit(1)
-	if os.path.isfile(sys.argv[2]):
-		print("File {} already exists".format(sys.argv[2]))
-		exit(1)
-	with open(sys.argv[1], 'r') as f:
+		if len(sys.argv) != 3:
+			print("Usage: ./markdown2html.py <markdown_file> <html_file>")
+			exit(1)
+		if not os.path.isfile(sys.argv[1]):
+			print("Missing {}".format(sys.argv[1]))
+			exit(1)
+		else:
+			exit(0)
+with open(sys.argv[1], 'r') as f:
 		text = f.read()
-	text = re.sub(r'\n\n', '\n', text)
-	text = re.sub(r'\n', '<br />\n', text)
-	text = re.sub(r'\*(.*)\*', r'<em>\1</em>', text)
-	text = re.sub(r'_(.*)_', r'<em>\1</em>', text)
-	text = re.sub(r'\[(.*)\]\((.*)\)', r'<a href="\2">\1</a>', text)
-	text = re.sub(r'\#(.*)', r'<h1>\1</h1>', text)
-	text = re.sub(r'\##(.*)', r'<h2>\1</h2>', text)
-	text = re.sub(r'\###(.*)', r'<h3>\1</h3>', text)
-	text = re.sub(r'\####(.*)', r'<h4>\1</h4>', text)
-	text = re.sub(r'\#####(.*)', r'<h5>\1</h5>', text)
-	text = re.sub(r'\######(.*)', r'<h6>\1</h6>', text)
-	text = re.sub(r'\> (.*)', r'<blockquote>\1</blockquote>', text)
+text = re.sub(r'\n\n', '\n', text)
+text = re.sub(r'\n', '<br />\n', text)
+text = re.sub(r'\*(.*)\*', r'<em>\1</em>', text)
+text = re.sub(r'_(.*)_', r'<em>\1</em>', text)
+text = re.sub(r'\[(.*)\]\((.*)\)', r'<a href="\2">\1</a>', text)
+text = re.sub(r'\#(.*)', r'<h1>\1</h1>', text)
+text = re.sub(r'\##(.*)', r'<h2>\1</h2>', text)
+text = re.sub(r'\###(.*)', r'<h3>\1</h3>', text)
+text = re.sub(r'\####(.*)', r'<h4>\1</h4>', text)
+text = re.sub(r'\#####(.*)', r'<h5>\1</h5>', text)
+text = re.sub(r'\######(.*)', r'<h6>\1</h6>', text)
+text = re.sub(r'\> (.*)', r'<blockquote>\1</blockquote>', text)
